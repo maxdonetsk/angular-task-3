c41ce08d4866dbe241b7ec26f6353a74bc399bc6
maxdonetsk <max.donetsk@mail.ru>
2016-11-11 22:17:25 +0200

    pagination for tile is done

--- app/tile/tile.controller.js
+++ app/tile/tile.controller.js
@@ -5,13 +5,24 @@
             .module('app')
             .controller('TileController', TileController);
 
-    function TileController(Items) {
+    function TileController(Items, ItemsPerPage) {
         var vm = this;
         vm.items = [];
+        vm.itemsPerPage = ItemsPerPage.values();
+        vm.itemsPerPage.selected = vm.itemsPerPage[0];
+        vm.currentPage = 1;
 
-        Items.getAll()
-                .then(function (response) {
-                    vm.items = response.data;
+        Items.getAllDummy()
+                .then(function () {
+                    for (var i = 1; i < 501; i++) {
+                        vm.items.push({
+                            id: i,
+                            title: 'Item ' + i,
+                            width: i * 8,
+                            height: i * 4,
+                            length: i * 7
+                        });
+                    }
                 });
     }
 }());
