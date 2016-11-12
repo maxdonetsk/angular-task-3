b8bb06baca4b4b64490c9c87e190d72e2bd729e5
maxdonetsk <max.donetsk@mail.ru>
2016-11-12 09:17:15 +0200

    getting items method refining

--- app/common/items.factory.js
+++ app/common/items.factory.js
@@ -14,7 +14,17 @@
                     }
 
                     function getAllDummy() {
-                        return $http.get('/data/items.json');
+                        var items = [];
+                        for (var i = 1; i < 501; i++) {
+                            items.push({
+                                id: i,
+                                title: 'Item ' + i,
+                                width: i * 8,
+                                height: i * 4,
+                                length: i * 7
+                            });
+                        }
+                        return items;
                     }
 
                     return  {
--- app/tile/tile.controller.js
+++ app/tile/tile.controller.js
@@ -7,22 +7,9 @@
 
     function TileController(Items, ItemsPerPage) {
         var vm = this;
-        vm.items = [];
+        vm.currentPage = 1;
         vm.itemsPerPage = ItemsPerPage.values();
         vm.itemsPerPage.selected = vm.itemsPerPage[0];
-        vm.currentPage = 1;
-
-        Items.getAllDummy()
-                .then(function () {
-                    for (var i = 1; i < 501; i++) {
-                        vm.items.push({
-                            id: i,
-                            title: 'Item ' + i,
-                            width: i * 8,
-                            height: i * 4,
-                            length: i * 7
-                        });
-                    }
-                });
+        vm.items = Items.getAllDummy();
     }
 }());
