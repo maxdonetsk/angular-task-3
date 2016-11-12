0f2155bd9283482585c283e0cf5256b4398f1baa
maxdonetsk <max.donetsk@mail.ru>
2016-11-12 12:16:11 +0200

    form reset implementation

--- app/item/item.controller.js
+++ app/item/item.controller.js
@@ -7,16 +7,27 @@
 
     function ItemController($stateParams, Items) {
         var vm = this;
+        var items = [];
+
         vm.header = '';
         vm.item = {};
-        
-        vm.header = $stateParams.id ? 'Edit item' : 'Create a new item';
+        vm.itemInitial = {};
+        vm.onCancel = onCancel;
+
         if (!!$stateParams.id) {
-            var items = Items.getAllDummy();
-            vm.item = items[$stateParams.id - 1];
+            items = Items.getAllDummy();
+            vm.item = angular.copy(items[$stateParams.id - 1]);
             vm.header = 'Edit item';
         } else {
             vm.header = 'Create a new item';
+        }
+
+        function onCancel() {
+            if (!!$stateParams.id && !!items) {
+                vm.item = angular.copy(items[$stateParams.id - 1]);
+            } else {
+                vm.item = {};
+            }
         }
     }
 }());
